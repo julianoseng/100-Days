80b4a8b07034fd6465a808283770d1fefa192800
julianoseng <66235816+julianoseng@users.noreply.github.com>
2021-04-07 22:05:39 -0500

    Day16 end but need to review lesson

---
+++ Day16/main.py
@@ -0,0 +1,40 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+
+is_on = True
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options})")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
+
+
+
+
+
+# from prettytable import PrettyTable
+#
+# table = PrettyTable()
+#
+# table.add_column("Pokemon Name",["Pikachu", "Squirtle", "Charmander"])
+# table.add_column("Type", ["Electric", "Water", "Fire"])
+#
+# table.align = "l"
+#
+#
+# print(table)
+
