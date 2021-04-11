a06ec68b9ffd9acf03c0633ce9493b9432cbd65d
Julian Oseng <julianoseng@gmail.com>
2021-04-11 13:50:44 -0500

    Day18 end

---
+++ Day18/hirst-painting/main.py
@@ -0,0 +1,16 @@
+# import colorgram
+#
+# image_colors = colorgram.extract('image.jpg', 15)
+#
+# colors = []
+# for color in image_colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     colors.append(new_color)
+#
+# print(colors)
+
+color_list = [(199, 175, 117), (124, 36, 24), (168, 106, 57), (186, 158, 53), (6, 57, 83), (109, 67, 85),
+              (113, 161, 175), (22, 122, 174), (64, 153, 138), (39, 36, 36), (76, 40, 48)]
--- Day18/main.py
+++ Day18/main.py
@@ -6,22 +6,46 @@ screen = Screen()
 tim.speed(0)
 tim_turn = [0, 90, 180, 270]
 screen.colormode(255)
+color_list = [(199, 175, 117), (124, 36, 24), (168, 106, 57), (186, 158, 53), (6, 57, 83), (109, 67, 85),
+              (113, 161, 175), (22, 122, 174), (64, 153, 138), (39, 36, 36), (76, 40, 48)]
+# turtle.dot(size=None, *color)
+tim.penup()
 
 
-def random_color():
-    r = random.randint(0, 255)
-    g = random.randint(0, 255)
-    b = random.randint(0, 255)
-    random_color = (r, g, b)
-    return random_color
+def hirst_painting():
+    tim.setheading(225)
+    tim.forward(300)
+    tim.setheading(0)
+    for i in range(10):
+        for i in range(10):
+            tim.dot(15, random.choice(color_list))
+            tim.forward(50)
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
 
-angle = 0
-for i in range(121):
-    tim.circle(120)
-    tim.setheading(angle)
-    tim.pencolor(random_color())
-    angle += 3
+hirst_painting()
+tim.hideturtle()
 
+# def random_color():
+#     r = random.randint(0, 255)
+#     g = random.randint(0, 255)
+#     b = random.randint(0, 255)
+#     color = (r, g, b)
+#     return color
+
+
+
+# # Draws a spirograph
+# def draw_spirograph(size_of_gap):
+#     for i in range(int(360 / size_of_gap)):
+#         tim.color(random_color())
+#         tim.circle(100)
+#         tim.setheading(tim.heading() + size_of_gap)
+#
+# draw_spirograph(5)
 
 ## Random path
 # for i in range(200):
