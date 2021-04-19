5be5856fbd567cdec7dd198d97694234a96ec18d
Julian Oseng <julianoseng@gmail.com>
2021-04-18 22:35:10 -0500

    Day 21 end

---
+++ Day20_Day21/scoreboard.py
@@ -0,0 +1,30 @@
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.goto(-10, 270)
+        self.color("white")
+        self.write(f"Score = {self.score}", False, align="center", font=("Arial", 16, "normal"))
+        self.ht()
+
+    def add_score(self):
+        self.score += 1
+        self.clear()
+        self.goto(0, 270)
+        self.write(f"Score = {self.score}", False, align="center", font=("Arial", 16, "normal"))
+
+    def restart_score(self):
+        self.score = 0
+        self.clear()
+        self.goto(-10, 270)
+        self.write(f"Score = {self.score}", False, align="center", font=("Arial", 16, "normal"))
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.ht()
+        self.color("white")
+        self.write("Game Over", False, align="center", font=("Arial", 20, "normal"))
