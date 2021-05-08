bc9739072ca46f5b73abcba4cf531fe32266a1fd
julianoseng <julianoseng@gmail.com>
2021-05-07 23:40:33 -0500

    Day 26 work

--- Day26/Challenge_3/main.py
+++ Day26/Challenge_3/main.py
@@ -1,4 +1,19 @@
 
+with open("file1.txt") as file_one:
+    numbers_one = file_one.readlines()
+
+with open("file2.txt") as file_two:
+    numbers_two = file_two.readlines()
+
+numbers_one = [int(num.strip()) for num in numbers_one]
+numbers_two = [int(num.strip()) for num in numbers_two]
+
+result = [int(num) for num in numbers_one if num in numbers_two]
+
+# for num in numbers_one:
+#     for numbers in numbers_two:
+#         if numbers == num:
+#             result.append(numbers)
 
 
 print(result)
--- Day26/main.py
+++ Day26/main.py
@@ -4,3 +4,8 @@
 # Conditional List Comprehension
 # new_list = [new_item for item in list if test]
 
+# Dictionary Comprehension
+# new_dict = {new_key:new_value for item in list}
+
+# Conditional Dictionary Comprehension
+# new_dict = {new_key:new_value for (key, value) in dict.items() if test}
